f5df42976492b8d93930df7606afd6ce8b750da6
aunogarafat <shadowman9443@gmail.com>
2020-01-21 02:04:32 +0600

    full project started using vue js

---
+++ Stock-Trader/src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <router-link to="/" class="navbar-brand">Stock Trader</router-link>
+      </div>
+      <ul class="nav navbar-nav">
+        <router-link to="/portfolio" activeClass="active" tag="li">
+          <a>Portfolio</a>
+        </router-link>
+        <router-link to="/stocks" activeClass="active" tag="li">
+          <a>Stocks</a>
+        </router-link>
+
+        <li>
+          <a href="#">Page 2</a>
+        </li>
+        <li>
+          <a href="#">Page 3</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
---
+++ Stock-Trader/src/components/stocks/Stocks.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <app-stock v-for="stock in stocks" v-bind:key="stock.id"></app-stock>
+    </div>
+</template>
+<script>
+import Stock from './Stock.vue'
+export default {
+    data(){
+        return {
+            stocks: [
+                {id:1, name: 'BMW' , price: 110},
+                {id:2, name: 'Tesla' , price: 110},
+                {id:3, name: 'Ford' , price: 110},
+                {id:4, name: 'Renault' , price: 110},
+            ]
+        }
+    },
+    components: {
+        appStock :Stock
+    }
+}
+</script>
